03b9668a704de5ef696774c6f30e06cedd47eeab
Metellus509 <metellusjunior56@gmail.com>
2025-09-05 20:08:32 +0000

    complete

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,38 @@
 class Song:
-    pass
+    genres        = []
+    artists       = []
+    genre_count   = {}
+    artist_count  = {}
+    count = 0
+
+    def __init__(self,name,artist,genre):
+        self.add_song_to_count()
+        self.name=name
+        self.artist=artist
+        self.genre=genre
+        Song.add_to_genres(self.genre)
+        Song.add_to_artists(self.artist)
+        Song.add_to_genre_count(self.genre)
+        Song.add_to_artists_count(self.artist)
+    
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count+=1
+    
+    @classmethod
+    def add_to_genres(cls,genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls,artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls,genre):
+        cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+
+    @classmethod
+    def add_to_artists_count(cls,artist):
+        cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
